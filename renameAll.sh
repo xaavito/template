f12b4901a31d9047b39043e7e0facd1430902c50
Javier Martin Gonzalez <javiermartingonzalez@gmail.com>
2021-03-25 16:01:08 -0300

    script para renombrado masivo

---
+++ renameAll.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf .git
+#sed -i "s/template/$1/g" pom.xml
+#sed -i "s/template/$1/g" *.java
+
+find . -name "*.pom" -exec sed -i "s/template/$1/g" {} +
+find . -name "*.java" -exec sed -i "s/template/$1/g" {} +
+
+find . -name "*.md" -exec sed -i "s.xaavito/template.xaavito/$1.g" {} +
+
+mv ../template "../$1"	
+
+cd ..
+cd "$1"
+git init
